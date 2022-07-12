6620ba35f655a96e6f41421504588e88f311f2b8
DeeBest <simphiwedladla8@gmail.com>
2022-07-12 06:37:36 +0200

    added a button a user can click and will be prompt number of boxes per side and create  a grid

--- script.js
+++ script.js
@@ -3,84 +3,52 @@ let body = document.querySelector('#body');
 let container = document.createElement('div');
     container.classList.add('container');
     body.appendChild(container);
+let numberOfBoxes = 16;
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-for (let i=0; i < 16; i++) {
-    let row1 = document.createElement('div');
-    row1.classList.add('box');
-    container.appendChild(row1);
+
+function eraseGrid () {
+    container.innerHTML = '';
 }
-for (let i=0; i < 16; i++) {
-    let row2 = document.createElement('div');
-    row2.classList.add('box');
-    container.appendChild(row2);
+
+function reloadGrid() {
+    eraseGrid()
+    createGrid(numberOfBoxes)
 }
-for (let i=0; i < 16; i++) {
-    let row3 = document.createElement('div');
-    row3.classList.add('box');
-    container.appendChild(row3);
-}
-for (let i=0; i < 16; i++) {
-    let row4 = document.createElement('div');
-    row4.classList.add('box');
-    container.appendChild(row4);
-}
-for (let i=0; i < 16; i++) {
-    let row5 = document.createElement('div');
-    row5.classList.add('box');
-    container.appendChild(row5);
-}
-for (let i=0; i < 16; i++) {
-    let row6 = document.createElement('div');
-    row6.classList.add('box');
-    container.appendChild(row6);
-}
-for (let i=0; i < 16; i++) {
-    let row7 = document.createElement('div');
-    row7.classList.add('box');
-    container.appendChild(row7);
-}
-for (let i=0; i < 16; i++) {
-    let row8 = document.createElement('div');
-    row8.classList.add('box');
-    container.appendChild(row8);
-}
-for (let i=0; i < 16; i++) {
-    let row9 = document.createElement('div');
-    row9.classList.add('box');
-    container.appendChild(row9);
-}
-for (let i=0; i < 16; i++) {
-    let row10 = document.createElement('div');
-    row10.classList.add('box');
-    container.appendChild(row10);
-}
-for (let i=0; i < 16; i++) {
-    let row11 = document.createElement('div');
-    row11.classList.add('box');
-    container.appendChild(row11);
-}
-for (let i=0; i < 16; i++) {
-    let row12 = document.createElement('div');
-    row12.classList.add('box');
-    container.appendChild(row12);
-}
-for (let i=0; i < 16; i++) {
-    let row13 = document.createElement('div');
-    row13.classList.add('box');
-    container.appendChild(row13);
-}
-for (let i=0; i < 16; i++) {
-    let row14 = document.createElement('div');
-    row14.classList.add('box');
-    container.appendChild(row14);
-}
-for (let i=0; i < 16; i++) {
-    let row15 = document.createElement('div');
-    row15.classList.add('box');
-    container.appendChild(row15);
-}
-for (let i=0; i < 16; i++) {
-    let row16 = document.createElement('div');
-    row16.classList.add('box');
-    container.appendChild(row16);
-}
+
+function boxErasor (e) {
+    e.target.style.backgroundColor = 'white';
+} 
+
+function createGrid(numberOfBoxes) {
+         container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 1fr)`
+         container.style.gridTemplateRows    = `repeat(${numberOfBoxes}, 1fr)`
+         for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.addEventListener('mouseover && mouseDown', () => {
+                box.style.backgroundColor = 'black'
+                }
+            );
+            container.appendChild(box);
+};
+};
+createGrid(numberOfBoxes);
+
+function userGrid (boxesAmount) {
+         boxesAmount = prompt('how many boxes per side?');
+         createGrid(boxesAmount);
+};
+
+let box = document.querySelector('.box');
+
+let button = document.createElement('button');
+    button.classList.add('userGridBtn');
+    button.textContent = ('Create Grid!');
+    button.addEventListener('click', () => {
+        eraseGrid();
+        userGrid();
+    })
+    body.appendChild(button);
