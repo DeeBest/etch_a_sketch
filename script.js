eb905eb0afb737a464ef1d37bfc32911c2110328
DeeBest <simphiwedladla8@gmail.com>
2022-07-12 12:59:58 +0200

    created buttons a user can use to create desired grid and added styles

--- script.js
+++ script.js
@@ -1,54 +1,116 @@
 'use strict'
 let body = document.querySelector('#body');
+
+let heading = document.createElement('h1');
+    heading.classList.add('h1');
+    heading.textContent = ('Etch A Sketch!');
+    body.appendChild(heading);
+
+let main = document.createElement('div');
+    main.classList.add('main');
+    body.appendChild(main);
+
+let footer = document.createElement('div');
+    footer.classList.add('footer');
+    footer.textContent = ('Created by Simphiwe');
+    body.appendChild(footer);
+
+let btnDiv = document.createElement('div');
+    btnDiv.classList.add('btnDiv');
+    main.appendChild(btnDiv);
+
 let container = document.createElement('div');
     container.classList.add('container');
-    body.appendChild(container);
-let numberOfBoxes = 16;
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+    main.appendChild(container);
 
+let numberOfBoxes = 16;
+
+let mouseDown = false
+    document.body.onmousedown = () => (mouseDown = true)
+    document.body.onmouseup = () => (mouseDown = false)
 
 function eraseGrid () {
     container.innerHTML = '';
 }
 
-function reloadGrid() {
-    eraseGrid()
-    createGrid(numberOfBoxes)
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    e.target.style.backgroundColor = 'black';
 }
-
-function boxErasor (e) {
-    e.target.style.backgroundColor = 'white';
-} 
 
 function createGrid(numberOfBoxes) {
          container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 1fr)`
          container.style.gridTemplateRows    = `repeat(${numberOfBoxes}, 1fr)`
          for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
             let box = document.createElement('div');
+            box.style.outline = ('1px solid black');
             box.classList.add('box');
-            box.addEventListener('mouseover && mouseDown', () => {
-                box.style.backgroundColor = 'black'
-                }
-            );
+            box.addEventListener('mousedown', changeColor);
+            box.addEventListener('mouseover', changeColor);
             container.appendChild(box);
 };
 };
 createGrid(numberOfBoxes);
 
-function userGrid (boxesAmount) {
-         boxesAmount = prompt('how many boxes per side?');
-         createGrid(boxesAmount);
-};
-
 let box = document.querySelector('.box');
 
-let button = document.createElement('button');
-    button.classList.add('userGridBtn');
-    button.textContent = ('Create Grid!');
-    button.addEventListener('click', () => {
+function userGrid (boxesAmount) {
+         boxesAmount = prompt('how many boxes per side Between 1 and 150?')
+         if (boxesAmount >=1 && boxesAmount <= 150) {
+            eraseGrid();
+            createGrid(boxesAmount);
+         } else {
+            alert('Please put the correct number between 1 and 150!!')
+            eraseGrid();
+            createGrid(numberOfBoxes);
+         }
+};
+  
+function hideGridLines (numberOfBoxes) {
+    numberOfBoxes = prompt('how many boxes per side Between 1 and 150?');
+    if (numberOfBoxes >= 1 && numberOfBoxes <= 150) {
+    eraseGrid();
+    container.style.gridTemplateColumns = `repeat(${numberOfBoxes}, 1fr)`
+    container.style.gridTemplateRows    = `repeat(${numberOfBoxes}, 1fr)`
+        for (let i = 0; i < numberOfBoxes * numberOfBoxes; i++) {
+            let box = document.createElement('div');
+                box.classList.add('box');
+                box.addEventListener('mousedown', changeColor);
+                box.addEventListener('mouseover', changeColor);
+                container.appendChild(box);
+         }
+    } else {
+        alert('Please put the correct amount between 1 and 150!!');
+        eraseGrid();
+        createGrid(numberOfBoxes);
+    }
+};
+
+let gridLinesBtn = document.createElement('button');
+    gridLinesBtn.setAttribute('id', 'gridLinesBtn');
+    gridLinesBtn.classList.add('button');
+    gridLinesBtn.textContent = ('Hide Grid Lines!');
+    gridLinesBtn.addEventListener('click', () => {
+        hideGridLines(numberOfBoxes);
+    });
+    btnDiv.appendChild(gridLinesBtn);
+
+let eraseGridBtn = document.createElement('button')
+    eraseGridBtn.setAttribute('id', 'eraseGridBtn');
+    eraseGridBtn.classList.add('button');
+    eraseGridBtn.textContent = ('Erase Grid!');
+    eraseGridBtn.addEventListener('click', () => {
+        eraseGrid();
+        createGrid(numberOfBoxes);
+    });
+    btnDiv.appendChild(eraseGridBtn);
+
+let createGridBtn = document.createElement('button');
+    createGridBtn.setAttribute('id', 'createGridBtn');
+    createGridBtn.classList.add('button');
+    createGridBtn.textContent = ('Create New Grid!');
+    createGridBtn.addEventListener('click', () => {
         eraseGrid();
         userGrid();
     })
-    body.appendChild(button);
+    btnDiv.appendChild(createGridBtn);
